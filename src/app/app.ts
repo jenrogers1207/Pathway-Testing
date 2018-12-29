441159eb4ec798ea1fff77a31c0408a3450d6561
Jen <jen@sci.utah.edu>
2018-12-28 23:25:57 -0700

    saving changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,10 @@ const dm = new DM.DataManager();
 //const queryAPI = DM.default;
 
 let test = dm.GET('hsa04910');
-/*
+
 let convertTest = dm.CONVERT('12190').then(d=> {
     console.log(d);
-    let test = JSON.parse(d);
+    //let test = JSON.parse(d);
     console.log(test)
-});*/
+});
 //console.log(convertTest);
