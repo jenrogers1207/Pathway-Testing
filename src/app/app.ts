772292d3b19c82e24d547d95439d3e74b3e1c38c
Jen <jen@sci.utah.edu>
2019-01-04 11:11:35 -0700

    added mygene info api request to query symbols

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import "./styles.scss";
 import * as DM from './dataManager';
 import * as d3 from 'D3';
 
+
 const xhr = require('nets');
 
 const dm = new DM.DataManager();
@@ -15,12 +16,39 @@ function searchById() {
     d3.select('#pathway-render').selectAll('*').remove();
 
     const value = (<HTMLInputElement>document.getElementById('search-bar')).value;
-   
-    if(value.includes('ncbi-geneid')){
-        dm.CONVERT(value);
+    if(value.includes(':')){
+        if(value.includes('ncbi-geneid')){
+            dm.CONVERT(value);
+        }else{
+            dm.LINK([value]);
+        }
     }else{
-        dm.LINK([value]);
+        let url = 'http://mygene.info/v3/query?q='+ value;
+        let proxy = 'https://cors-anywhere.herokuapp.com/';
+     
+        let data = xhr({
+                url: proxy + url,
+                method: 'GET',
+                encoding: undefined,
+                headers: {
+                    "Content-Type": "application/json"
+                
+                }
+            }, 
+            function done(err, resp, body){
+                
+                if(err){ 
+                    console.error(err); 
+                    return;
+                }
+         
+                let json = JSON.parse(resp.rawRequest.responseText);
+                
+                
+                dm.CONVERT('ncbi-geneid:'+json.hits[0]._id);
+            });
     }
+        
 
   
 }
